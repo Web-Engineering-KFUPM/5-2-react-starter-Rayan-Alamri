34d5961fc0d46fcd6d7841f88515808796ae4720
Rayan-Alamri <s202247900@kfupm.edu.sa>
2025-10-01 09:11:52 +0300

    task 1 finshed

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,10 @@
+function StudentCard(){
+    return(
+        <div>
+        <h3>Name: Rayan</h3>
+        <p>ID: 202247900</p>
+        <p>Department: ICS</p>
+        </div>
+    );
+}
+export default StudentCard;
